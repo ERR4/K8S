ba72fff49242ca0f68d0e998818486bc55f66680
RaghuRam Reddy <raghurr.devops@gmail.com>
2022-10-09 12:01:15 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,7 +3,7 @@ file=$1
 if [ "${file: -4}" == ".yml" ] || if [ "${file: -5}" == ".yaml" ]
 then 
 	echo "Yes! 
-	"kubectl apply -f "${{ steps.abc.outputs.added }}"
+/	kubectl apply -f $file
 else 
 	echo "no!"
 fi
