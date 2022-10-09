f339410781e540e694a94efbbbdfefa6d1de64f9
RaghuRam Reddy <raghurr.devops@gmail.com>
2022-10-09 12:34:05 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 file=$1
-if [ "${file: -4}" == ".yml" ] || if [ "${file: -5}" == ".yaml" ]
+if [ ${file: -4} == ".yml" ] || [ ${file: -5} == '.yaml' ]
 then 
-	echo "Yes!"
-#	kubectl apply -f $file
+	echo "Yes!" ;
+	echo "kubectl apply -f $file";
 else 
-	echo "no!"
+	echo "no!";
 fi
